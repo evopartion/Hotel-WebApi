6209603210fc68480e01b48cae1032a9ece1d920
evopartion <61056779+evopartion@users.noreply.github.com>
2025-03-04 03:38:07 +0300

    Services ve Managers

---
+++ ApiConsume/HotelProject.BusinessLayer/Concrete/TestimonialManager.cs
@@ -0,0 +1,43 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.DataAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.BusinessLayer.Concrete
+{
+    public class TestimonialManager : ITestimonialService
+    {
+        private readonly ITestimonialDal _testimonialDal;
+        public TestimonialManager(ITestimonialDal testimonialDal)
+        {
+            _testimonialDal = testimonialDal;
+        }
+        public void TDelete(EntityLayer.Concrete.Testimonial t)
+        {
+            _testimonialDal.Delete(t);
+        }
+
+        public EntityLayer.Concrete.Testimonial TGetByID(int id)
+        {
+            return _testimonialDal.GetByID(id);
+        }
+
+        public List<EntityLayer.Concrete.Testimonial> TGetList()
+        {
+            return _testimonialDal.GetList();
+        }
+
+        public void TInsert(EntityLayer.Concrete.Testimonial t)
+        {
+            _testimonialDal.Insert(t);
+        }
+
+        public void TUpdate(EntityLayer.Concrete.Testimonial t)
+        {
+            _testimonialDal.Update(t);
+        }
+    }
+}
