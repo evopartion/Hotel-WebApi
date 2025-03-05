8c45e079082fe15b7c955c5e6e2263e345a8b10c
evopartion <61056779+evopartion@users.noreply.github.com>
2025-03-06 02:57:24 +0300

    Hizmetler

---
+++ Frontend/HotelProject.WebUI/Dtos/ServiceDto/CreateServiceDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.ServiceDto
+{
+    public class CreateServiceDto
+    {
+        [Required(ErrorMessage="Boş Bırakılamaz")]
+        public string ServiceIcon { get; set; }
+        [Required(ErrorMessage = "Boş Bırakılamaz")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Boş Bırakılamaz")]
+        public string Description { get; set; }
+    }
+
+}
---
+++ Frontend/HotelProject.WebUI/Dtos/ServiceDto/UpdateServiceDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.ServiceDto
+{
+    public class UpdateServiceDto
+    {
+        public int ServiceID { get; set; }
+        [Required(ErrorMessage = "Boş Bırakılamaz")]
+        public string ServiceIcon { get; set; }
+        [Required(ErrorMessage = "Boş Bırakılamaz")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Boş Bırakılamaz")]
+        public string Description { get; set; }
+    }
+}
