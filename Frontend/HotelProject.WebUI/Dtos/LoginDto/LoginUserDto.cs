cb0455853d832bd3bfd93f910f36128c6d8d7043
evopartion <61056779+evopartion@users.noreply.github.com>
2025-03-06 04:58:03 +0300

    Login

---
+++ Frontend/HotelProject.WebUI/Dtos/LoginDto/LoginUserDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.LoginDto
+{
+    public class LoginUserDto
+    {
+        [Required(ErrorMessage ="boş bırakılamaz")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "boş bırakılamaz")]
+        public string Password { get; set; }
+    }
+}
