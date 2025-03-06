7fb132906fb3e99e6f0506c2d03dbdc38abe0617
evopartion <61056779+evopartion@users.noreply.github.com>
2025-03-06 04:45:46 +0300

    Register

--- ApiConsume/HotelProject.EntityLayer/Concrete/AppUser.cs
+++ ApiConsume/HotelProject.EntityLayer/Concrete/AppUser.cs
@@ -10,7 +10,7 @@ namespace HotelProject.EntityLayer.Concrete
     public class AppUser:IdentityUser<int>
     {
         public string Name { get; set; }
-        public string SurName { get; set; }
+        public string Surname { get; set; }
         public string City { get; set; }
     }
 }
