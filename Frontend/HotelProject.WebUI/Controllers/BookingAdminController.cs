1875f57cb8017fd8cdf0914fa6f44b7a9f46bec7
evopartion <61056779+evopartion@users.noreply.github.com>
2025-03-07 06:43:01 +0300

    Admin Booking Pages

---
+++ Frontend/HotelProject.WebUI/Controllers/BookingAdminController.cs
@@ -0,0 +1,43 @@
+﻿using HotelProject.WebUI.Dtos.BookingDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class BookingAdminController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public BookingAdminController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:54005/api/Booking");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBookingDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        public async Task<IActionResult> ApprovedReservation2(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"http://localhost:54005/api/Booking/BookingAproved?id={id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
+    }
+}
--- Frontend/HotelProject.WebUI/Dtos/BookingDto/CreateBookingDto.cs
+++ Frontend/HotelProject.WebUI/Dtos/BookingDto/CreateBookingDto.cs
@@ -7,7 +7,7 @@ namespace HotelProject.WebUI.Dtos.BookingDto
         public string Name { get; set; }
         public string Mail { get; set; }
         public DateTime Checkin { get; set; }
-        public DateTime Checkout { get; set; }
+        public DateTime CheckOut { get; set; }
         public string AdultCount { get; set; }
         public string ChildCount { get; set; }
         public string RoomCount { get; set; }
