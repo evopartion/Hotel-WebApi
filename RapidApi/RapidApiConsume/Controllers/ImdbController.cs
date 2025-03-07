f1aa10fbdd85d594a0ebf787f4ae75a51e9459cb
evopartion <61056779+evopartion@users.noreply.github.com>
2025-03-08 01:18:03 +0300

    Rapid Api Projeye Dahil Etme

---
+++ RapidApi/RapidApiConsume/Controllers/ImdbController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System;
+using System.Net.Http.Headers;
+
+using System.Threading.Tasks;
+
+namespace RapidApiConsume.Controllers
+{
+    public class ImdbController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/series/"),
+                Headers =
+    {
+        { "x-rapidapi-key", "d25fea1d2emsha784d5dba56201bp1628e8jsn9ed1bab4b844" },
+        { "x-rapidapi-host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                
+            }
+            return View();
+        }
+    }
+}
