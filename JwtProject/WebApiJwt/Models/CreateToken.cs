a557c6f9c5f141674c691115fc29bc60dc7579b5
evopartion <61056779+evopartion@users.noreply.github.com>
2025-03-08 04:42:02 +0300

    Admin Token Oluşturma

--- JwtProject/WebApiJwt/Models/CreateToken.cs
+++ JwtProject/WebApiJwt/Models/CreateToken.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.IdentityModel.Tokens;
 using System;
+using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 
 namespace WebApiJwt.Models
@@ -16,6 +18,23 @@ namespace WebApiJwt.Models
                 notBefore: DateTime.Now, expires: DateTime.Now.AddMinutes(3), signingCredentials: credentials);
             JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
             return handler.WriteToken(token);
+        }
+
+        public string TokenCreateAdmin()
+        {
+            var bytes = Encoding.UTF8.GetBytes("aspnetcoreapiapiaspnetcoreapiapiaspnetcoreapiapi");
+            SymmetricSecurityKey key=new SymmetricSecurityKey(bytes);
+            SigningCredentials credentials=new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.NameIdentifier,Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.Role,"Admin"),
+                new Claim(ClaimTypes.Role,"Visitor")
+            };
+            JwtSecurityToken jwtSecurityToken = new JwtSecurityToken(issuer: "http://localhost", audience: "http://localhost", notBefore: DateTime.Now, expires: DateTime.Now.AddSeconds(30), signingCredentials: credentials, claims: claims);
+
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            return handler.WriteToken(jwtSecurityToken);
         }
     }
 }
