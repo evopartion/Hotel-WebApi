adf4bd61a0d89b4698c09ee8d58fa6409845a975
evopartion <61056779+evopartion@users.noreply.github.com>
2025-03-08 07:47:14 +0300

    Contact Page

---
+++ Frontend/HotelProject.WebUI/Controllers/ContactController.cs
@@ -0,0 +1,43 @@
+﻿using HotelProject.WebUI.Dtos.ContactDto;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public ContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            
+            return View();
+        }
+
+        [HttpGet]
+        public PartialViewResult SendMessage()
+        {
+
+            return PartialView();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SendMessage(CreateContactDto createContactDto)
+        {
+            createContactDto.Date = DateTime.Parse(DateTime.Now.ToShortDateString());
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createContactDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            await client.PostAsync("http://localhost:54005/api/Contact", stringContent);
+            return RedirectToAction("Index", "Default");
+        }
+    }
+}
