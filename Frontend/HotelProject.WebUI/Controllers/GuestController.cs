3af07ee786d93e168687d9303b959ef7664910fc
evopartion <61056779+evopartion@users.noreply.github.com>
2025-03-09 21:49:56 +0300

    Fluent Validation

--- Frontend/HotelProject.WebUI/Controllers/GuestController.cs
+++ Frontend/HotelProject.WebUI/Controllers/GuestController.cs
@@ -66,13 +66,17 @@ namespace HotelProject.WebUI.Controllers
         [HttpGet]
         public async Task<IActionResult> UpdateGuest(int id)
         {
-            var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync($"http://localhost:54005/api/Guest/{id}");
-            if (responseMessage.IsSuccessStatusCode)
+            if (ModelState.IsValid)
             {
-                var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<UpdateGuestDto>(jsonData);
-                return View(values);
+                var client = _httpClientFactory.CreateClient();
+                var responseMessage = await client.GetAsync($"http://localhost:54005/api/Guest/{id}");
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                    var values = JsonConvert.DeserializeObject<UpdateGuestDto>(jsonData);
+                    return View(values);
+                }
+            
             }
             return View();
         }
