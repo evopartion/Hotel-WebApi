f3156f4af19e5d99fba69aee0708b57f54319b37
evopartion <61056779+evopartion@users.noreply.github.com>
2025-03-14 18:32:00 +0300

    Dashboarda Veri Taşıma

--- Frontend/HotelProject.WebUI/ViewComponents/Dashboard/_DashboardWidgetPartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Dashboard/_DashboardWidgetPartial.cs
@@ -1,11 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace HotelProject.WebUI.ViewComponents.Dashboard
 {
-    public class _DashboardWidgetPartial: ViewComponent
+    public class _DashboardWidgetPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _DashboardWidgetPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:54005/api/DashboardWidgets/StaffCount");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData= await responseMessage.Content.ReadAsStringAsync();
+                ViewBag.v = jsonData;
+            }
+            
+
             return View();
         }
     }
